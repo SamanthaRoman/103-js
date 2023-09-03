09a5c2b9ade794ff10f9e4ed47b232f698fa1932
Samantha Roman <samantharoman@Samanthas-MBP-2.lan>
2023-09-03 14:58:41 -0400

    calculator

--- calculator/app.js
+++ calculator/app.js
@@ -1,13 +1,46 @@
-function calculator(){
-    if(sign == "+"){
-        num1 + num2
+
+
+
+
+function calculate(){
+
+let num1=document.getElementById("num1").value;
+let num2=document.getElementById("num2").value;
+// let sign=document.getElementById("txtOpp").value;
+let opp = document.getElementById("opperator").value;
+
+
+    if( opp === "+"){
+        const result = parseInt(num1) + parseInt(num2);
+        document.getElementById("total").innerHTML=`
+        <p> ${num1} ${opp} ${num2} = ${result}</p>`;
+        console.log(result); 
+        
     }
 
-    if(sign == "-")
+    if( opp === "-"){
+        const result = parseInt(num1) - parseInt(num2) ;
+        document.getElementById("total").innerHTML=`
+        <p> ${num1} ${opp} ${num2} = ${result}</p>`;
+        console.log(result); 
+    }
 
-    if(sign == "*")
+    if( opp === "*"){
+        const result = parseInt(num1) * parseInt(num2) ;
+        document.getElementById("total").innerHTML=`
+        <p> ${num1} ${opp} ${num2} = ${result}</p>`;
+        console.log(result); 
+    }
 
-    if(sign == "/")
+    if( opp === "/"){
+        const result = parseInt(num1) / parseInt(num2) ;
+        document.getElementById("total").innerHTML=`
+        <p> ${num1} ${opp} ${num2} = ${result}</p>`;
+        console.log(result); 
+    };
+
+};
 
 
-}
+
+// calculate();
